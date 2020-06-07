94b808a15e40ba070789b0f3acd09e693c6fbf0a
Sarah Ferreira <sarahfssilva@hotmail.com>
2020-06-07 14:08:12 -0300

    backend / frontend

---
+++ web/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css'
+
+import Routes from './routes'
+
+// JSX: sintaxe de XML dentro do JavaScript
+
+function App() {
+
+  return (
+    <Routes/>
+  );
+}
+
+export default App;
